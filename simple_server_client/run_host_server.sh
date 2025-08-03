4ff8e9834a277ed3430c07152d98572eb268415f
PXT151120 <thuong15112000@gmail.com>
2025-08-03 20:51:49 +0700

    add debug

--- simple_server_client/run_host_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the directory where this script is located
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Set the library path and run host_server
-export LD_LIBRARY_PATH="$SCRIPT_DIR/install/usr/lib:$LD_LIBRARY_PATH"
-exec "$SCRIPT_DIR/install/usr/bin/host_server" "$@"
